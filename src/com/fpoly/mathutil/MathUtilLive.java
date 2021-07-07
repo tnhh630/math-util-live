15b73c02ed69733871129e108c8e3d22b0ef99b7
tnhh630 <tnhh630@gmail.com>
2021-07-07 17:50:36 +0700

    Lan thu â lam chuyen ay

--- src/com/fpoly/mathutil/MathUtilLive.java
+++ src/com/fpoly/mathutil/MathUtilLive.java
@@ -20,6 +20,10 @@ public class MathUtilLive {
         System.out.println("expected: 5! = 120; actual: " + result);
         System.out.println("expected: 6! = 720; actual: " + MathUtil.getFactorial(6));
         System.out.println("expected: 0! = 1; actual: " + MathUtil.getFactorial(0));
+        
+        //THÊM CODE SAO LẦN ĐẦU TIÊN LÀM CHUYỆN ẤY 5:49 PM 7/7/2021
+        System.out.println("expected: 1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
 
 //        MathUtil.getFactorial(-5); - ném ngoại lệ
         // kĩ thuật kiểm thử phần mềm : ước lượng xem giá trị trả về  của hàm là gì
